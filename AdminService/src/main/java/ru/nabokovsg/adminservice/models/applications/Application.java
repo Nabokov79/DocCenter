fb8b225cbeb09f4b562d71d5f00c63dba65563ee
NabokovSG <nabokovsg@yandex.ru>
2023-02-17 14:32:27 +0300

    add new model

---
+++ AdminService/src/main/java/ru/nabokovsg/adminservice/models/applications/Application.java
@@ -0,0 +1,57 @@
+package ru.nabokovsg.adminservice.models.applications;
+
+import lombok.*;
+import ru.nabokovsg.adminservice.Type;
+import ru.nabokovsg.adminservice.models.addresses.Address;
+import ru.nabokovsg.adminservice.models.tanks.Orientation;
+import ru.nabokovsg.adminservice.models.users.User;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "applications")
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "date")
+    private LocalDate date;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+    @Column(name = "work")
+    private String work;
+    @Column(name = "passport_type")
+    @Enumerated(EnumType.STRING)
+    private PassportType passportType;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "type_id", referencedColumnName = "id")
+    private Type type;
+    @Column(name = "number")
+    private Integer number;
+    @Column(name = "orientation")
+    @Enumerated(EnumType.STRING)
+    private Orientation orientation;
+    @Column(name = "volume")
+    private Integer volume;
+    @Column(name = "primary_visit")
+    private Boolean primary;
+    @Column(name = "repeat_visit")
+    private Boolean repeat;
+    @Column(name = "note")
+    private String note;
+    @ManyToMany(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "applications_users",
+            joinColumns =  {@JoinColumn(name = "application_id")},
+            inverseJoinColumns = {@JoinColumn(name = "user_id")})
+    @ToString.Exclude
+    private List<User> users;
+}
---
+++ AdminService/src/main/java/ru/nabokovsg/adminservice/models/applications/PassportType.java
@@ -0,0 +1,10 @@
+package ru.nabokovsg.adminservice.models.applications;
+
+public enum PassportType {
+
+    BOILER,
+    TANK,
+    PIPELINE,
+    OIL_PIPELINE,
+    FILTER
+}
