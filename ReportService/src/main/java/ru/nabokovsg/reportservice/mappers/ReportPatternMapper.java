36620bd39019fd1c8591e980e0ca5bf50d5e7098
NabokovSG <nabokovsg@yandex.ru>
2023-03-03 15:08:26 +0300

    add mapper class

---
+++ ReportService/src/main/java/ru/nabokovsg/reportservice/mappers/ReportPatternMapper.java
@@ -0,0 +1,17 @@
+package ru.nabokovsg.reportservice.mappers;
+
+import org.mapstruct.Mapper;
+import ru.nabokovsg.reportservice.dtos.NewReportPatternDto;
+import ru.nabokovsg.reportservice.dtos.ReportPatternDto;
+import ru.nabokovsg.reportservice.dtos.UpdateReportPatternDto;
+import ru.nabokovsg.reportservice.models.ReportPattern;
+
+@Mapper(componentModel = "spring")
+public interface ReportPatternMapper {
+
+    ReportPattern mapToReportPattern(NewReportPatternDto patternDto);
+
+    ReportPatternDto mapToReportPatternDto(ReportPattern patternDto);
+
+    ReportPattern fromUpdateReportPattern(UpdateReportPatternDto patternDto);
+}
