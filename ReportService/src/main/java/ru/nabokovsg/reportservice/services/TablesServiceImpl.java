788bbcbfb2f2e344ce6bfb1024486876081038c2
NabokovSG <nabokovsg@yandex.ru>
2023-03-06 16:06:54 +0300

    update methods

--- ReportService/src/main/java/ru/nabokovsg/reportservice/services/TablesServiceImpl.java
+++ ReportService/src/main/java/ru/nabokovsg/reportservice/services/TablesServiceImpl.java
@@ -13,6 +13,8 @@ public class TablesServiceImpl implements TablesService {
 
     private final TablesRepository repository;
     private final TableColumnsRepository columnsRepository;
+    private final SubTableService subTableService;
+    private final ElementService elementService;
 
     @Override
     public void save(Tables tables) {
@@ -22,6 +24,11 @@ public class TablesServiceImpl implements TablesService {
             columns.setTables(tablesDb);
         }
         columnsRepository.saveAll(tablesDb.getTableColumns());
+        if (tables.getElements() != null) {
+            elementService.saveFromTable(tablesDb, tables.getElements());
+        } else if (tables.getSubTables() != null){
+            subTableService.save(tablesDb,tables.getSubTables());
+        }
     }
 }
 
