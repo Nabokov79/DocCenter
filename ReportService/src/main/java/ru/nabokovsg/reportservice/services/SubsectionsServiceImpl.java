89439fbfe64598421ed2380834de26f6191cd1bf
NabokovSG <nabokovsg@yandex.ru>
2023-03-06 16:06:35 +0300

    update methods

--- ReportService/src/main/java/ru/nabokovsg/reportservice/services/SubsectionsServiceImpl.java
+++ ReportService/src/main/java/ru/nabokovsg/reportservice/services/SubsectionsServiceImpl.java
@@ -14,19 +14,18 @@ public class SubsectionsServiceImpl implements SubsectionsService {
 
     private final SubsectionsRepository subsectionsRepository;
     private final TablesService tablesService;
-    private final ConstantService constantService;
 
     @Override
     public void save(Sections section, List<Subsections> subsections) {
-        for (Subsections subsection : subsections) {
-            subsection.setSections(section);
-            if (subsection.getTables() != null) {
-                tablesService.save(subsection.getTables());
+        if (!subsections.isEmpty()) {
+            for (Subsections subsection : subsections) {
+                subsection.setSections(section);
+                if (subsection.getTables() != null) {
+                    tablesService.save(subsection.getTables());
+                }
             }
-            if (subsection.getConstant() != null) {
-                constantService.save(subsection.getConstant());
-            }
+            subsectionsRepository.saveAll(subsections);
         }
-        subsectionsRepository.saveAll(subsections);
+
     }
 }
