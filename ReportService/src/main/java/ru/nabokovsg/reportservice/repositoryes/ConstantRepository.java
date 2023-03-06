2823e245095b4642d8100da4c7997afe4027e4ae
Sergey Nabokov <97031657+Nabokov79@users.noreply.github.com>
2023-03-06 16:41:31 +0300

    Delete ConstantRepository.java

--- ReportService/src/main/java/ru/nabokovsg/reportservice/repositoryes/ConstantRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nabokovsg.reportservice.repositoryes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokovsg.reportservice.models.Constant;
-
-public interface ConstantRepository extends JpaRepository<Constant, Long> {
-}
