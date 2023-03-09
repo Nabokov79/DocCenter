1144b4e75270c3f47d51440275b828a9a3b420ff
Sergey Nabokov <97031657+Nabokov79@users.noreply.github.com>
2023-03-09 16:53:54 +0300

    Delete TableColumnsRepository.java

--- ReportService/src/main/java/ru/nabokovsg/reportservice/repositoryes/TableColumnsRepository.java
+++
@@ -1,6 +0,0 @@
-package ru.nabokovsg.reportservice.repositoryes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TableColumnsRepository extends JpaRepository<TableColumns, Long> {
-}
